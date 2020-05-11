d25eaeef608ed0f2ba9671e13f6c226a9403be08
Madison McCardle <madison.mccardle@powerhrg.com>
2020-05-11 16:03:27 -0400

    errors

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  attr_accessor :side_a, :side_b, :side_c
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind
+    validate_triangle
+    if side_a == side_b && side_b == side_c
+      :equilateral
+    elsif side_a == side_b || side_b == side_c || side_a == side_c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    real_tri = [(side_a + side_b > side_c), (side_b + side_c > side_a), (side_a + side_c > side_b)]
+    [side_a, side_b, side_c].each do |side|
+      if side <= 0
+        real_tri << false
+      end
+      if real_tri.include?(false)
+        raise TriangleError
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
+
