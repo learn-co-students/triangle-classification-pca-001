9c961cfc788bc19b8466a0376998be3a307f17c3
Pat McClernan <patmccler@gmail.com>
2020-05-11 15:06:55 -0400

    throws custom error if triangle is invalid

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    raise TriangleError unless valid_triangle
+
+    if @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def valid_triangle
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+      false
+    elsif (@side1 + @side2 <= @side3) || (@side2 + @side3 <= @side1) || (@side3 + @side1 <= @side2)
+      false
+    else
+      true
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      puts "Not a valid triangle"
+    end
+  end
 end
