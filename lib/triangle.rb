a0daab0f4600091b917270f7fbdcdba0fd08da8e
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-05-11 15:13:46 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,44 @@
+require "pry"
+
 class Triangle
-  # write code here
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if valid? != true
+      raise TriangleError
+    else
+      return :equilateral if equilateral?
+      return :isosceles if isosceles?
+      return :scalene if scalene?
+    end
+  end
+
+  def equilateral?
+    @side1 == @side2 && @side2 == @side3
+  end
+
+  def isosceles?
+    @side2 == @side3 || @side1 == @side3 || @side1 == @side2
+  end
+
+  def scalene?
+    @side1 != @side2 && @side1 != @side3 && @side2 != @side3
+  end
+
+  def valid?
+     @side1.positive? && @side2.positive? && @side3.positive? && inequality?
+  end
+
+  def inequality?
+    (@side1 + @side2) > @side3 && (@side1 + @side3) > @side2 && (@side2 + @side3) > @side1
+  end
+
+  class TriangleError < StandardError
+  end
 end
